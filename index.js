0c8d846f3bf628d6ad06de22452a0ba5ad259352
aviraj83405147 <aviraj8340514701@gmail.com>
2025-06-27 02:34:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import login from "fca-priyansh";
 import fs from "fs";
 import express from "express";
 
-const OWNER_UIDS = ["100069692356853", "100005122337500"];
+const OWNER_UIDS = ["100069692356853", "100069692356853",  "100005122337500"];
 const friendUIDs = fs.existsSync("Friend.txt")
   ? fs.readFileSync("Friend.txt", "utf8").split("\n").map(x => x.trim())
   : [];
